910f95ea3fa9bd4807b13ba424b5e7a07606fc31
xdave3 <10771055+xdave777@users.no.reply.github com>
2023-10-07 20:13:40 +0200

    Add active link styling

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,4 +1,4 @@
-import { Outlet, Link,  useLoaderData, Form, redirect} from "react-router-dom";
+import { Outlet, NavLink, useLoaderData, Form, redirect} from "react-router-dom";
 import { createContact, getContacts } from "../contacts";
 
 export async function action() {
@@ -46,7 +46,16 @@ export default function Root() {
             <ul>
               {contacts.map((contact) => (
                 <li key={contact.id}>
-                  <Link to={`contacts/${contact.id}`}>
+                  <NavLink
+                      to={`contacts/${contact.id}`}
+                      className={({ isActive, isPending }) =>
+                        isActive
+                          ? "active"
+                          : isPending
+                          ? "pending"
+                          : ""
+                      }
+                  >
                     {contact.first || contact.last ? (
                       <>
                         {contact.first} {contact.last}
@@ -55,7 +64,8 @@ export default function Root() {
                       <i>No Name</i>
                     )}{" "}
                     {contact.favorite && <span>★</span>}
-                  </Link>
+                      {/* other code */}
+                    </NavLink> 
                 </li>
               ))}
             </ul>
