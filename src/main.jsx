aa0b6fc5c38fcda2135a46867456ab503a7349f6
xdave3 <10771055+xdave777@users.no.reply.github com>
2023-10-07 20:28:37 +0200

    Add index router

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,8 @@ import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
+
 
 const router = createBrowserRouter([
   {
@@ -22,6 +24,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
