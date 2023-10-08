50af3dc9623983e1ef45d07cba38e7b83c345855
xdave3 <10771055+xdave777@users.no.reply.github com>
2023-10-08 02:06:54 +0200

    Add managing the history stack

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -44,8 +44,11 @@ export default function Root() {
                 name="q"
                 defaultValue={q}
                 onChange={(event) => {
-                  submit(event.currentTarget.form);
-                }}
+                  const isFirstSearch = q == null;
+                  submit(event.currentTarget.form, {
+                    replace: !isFirstSearch,
+                  });
+                  }}
               />
               <div
                 id="search-spinner"
