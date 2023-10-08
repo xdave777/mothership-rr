5ae2bddd0a1bbbdb88d739e93e99afa9cdf77079
xdave3 <10771055+xdave777@users.no.reply.github com>
2023-10-08 03:04:07 +0200

    Add favorite star functionality

--- src/routes/contact.jsx
+++ src/routes/contact.jsx
@@ -1,10 +1,18 @@
-import { Form, useLoaderData } from "react-router-dom";
-import { getContact } from "../contacts";
+import { Form, useLoaderData, useFetcher } from "react-router-dom";
+import { getContact, updateContact } from "../contacts";
+
+export async function action({ request, params }) {
+  let formData = await request.formData();
+  return updateContact(params.contactId, {
+    favorite: formData.get("favorite") === "true",
+  });
+}
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
   return { contact };
 }
+
 
 export default function Contact() {
   const { contact } = useLoaderData();
@@ -70,11 +78,16 @@ export default function Contact() {
 
 // eslint-disable-next-line react/prop-types
 function Favorite({ contact }) {
+  const fetcher = useFetcher();
+
   // yes, this is a `let` for later
   // eslint-disable-next-line react/prop-types
   let favorite = contact.favorite;
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get("favorite") === "true";
+  }
   return (
-    <Form method="post">
+    <fetcher.Form method="post">
       <button
         name="favorite"
         value={favorite ? "false" : "true"}
@@ -86,6 +99,6 @@ function Favorite({ contact }) {
       >
         {favorite ? "★" : "☆"}
       </button>
-    </Form>
+    </fetcher.Form>
   );
 }
